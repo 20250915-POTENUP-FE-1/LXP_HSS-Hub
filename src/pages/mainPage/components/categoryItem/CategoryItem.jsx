a392e5bc930e40952d04dc869f13ad3561336818
joyh7680 <joy.h7680@gmail.com>
2025-11-06 21:25:41 +0900

    feat : 메인페이지 레이아웃 및 컴포넌트 기본 ui 작성

---
+++ src/pages/mainPage/components/categoryItem/CategoryItem.jsx
@@ -0,0 +1,15 @@
+import './CategoryItem.css';
+
+function CategoryItem() {
+  return (
+    <div>
+      <button>카테고리버튼</button>
+      <button>카테고리버튼</button>
+      <button>카테고리버튼</button>
+      <button>카테고리버튼</button>
+      <button>카테고리버튼</button>
+    </div>
+  );
+}
+
+export default CategoryItem;
--- src/pages/mainPage/components/searchBar/SearchBar.jsx
+++ src/pages/mainPage/components/searchBar/SearchBar.jsx
@@ -1,7 +1,20 @@
 import './SearchBar.css';
 
 function SearchBar() {
-  return <div>SearchBar</div>;
+  return (
+    <div className="search-bar">
+      <input
+        type="search"
+        name="keyword"
+        placeholder="검색어를 입력해주세요"
+        // value={}
+        // onChange={}
+      />
+      <button>
+        {/* <SearchIcon /> */}
+        검색아이콘 @
+      </button>
+    </div>
+  );
 }
-
 export default SearchBar;
