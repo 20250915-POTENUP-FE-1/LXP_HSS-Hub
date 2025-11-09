42b0ed4586b7f4facb9a8f8e2b251fb6683ee1d8
joyh7680 <joy.h7680@gmail.com>
2025-11-09 15:43:59 +0900

    feat: 검색창 구현, 메인페이지 수정

--- src/pages/mainPage/components/searchBar/SearchBar.jsx
+++ src/pages/mainPage/components/searchBar/SearchBar.jsx
@@ -1,31 +1,15 @@
 import { useState } from 'react';
 import './SearchBar.css';
 import { Search } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
-import { menuData } from '../../../../data/dummy';
 
-function SearchBar() {
-  // input 검색어 상태관리
-  const [searchInput, setSearchInput] = useState({
-    keyword: '',
-  });
+function SearchBar({ onSearch }) {
+  const [keyword, setKeyword] = useState('');
 
-  //  검색결과 상태관리 (결과 표시용)
-  const [searchResult, setSearchResult] = useState([])
-
-  const navigate = useNavigate();
-
-  // input 이벤트
-  const handleSearchInputChange = (e) => {
-    setSearchInput({
-      ...searchInput,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  //나중에 더미데이터 파일 만들어지면 수정 @@
-  const handleSearch = () => {
-    navigate(` `);
+  // 엔터 키로도 검색 가능
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      onSearch(keyword);
+    }
   };
 
   return (
@@ -34,14 +18,16 @@ function SearchBar() {
         type="search"
         name="keyword"
         className="search-bar-input"
-        value={searchInput.keyword}
-        onChange={handleSearchInputChange}
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+        onKeyDown={handleKeyPress}
         placeholder="검색어를 입력해주세요"
       />
-      <button className="search-bar-button" onClick={handleSearch}>
+      <button className="search-bar-button" onClick={() => onSearch(keyword)}>
         <Search size={20} color="#ffffff" />
       </button>
     </div>
   );
 }
+
 export default SearchBar;
