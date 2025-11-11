c8f0d04fe847aff219f85a34d1774d9f06c680a7
seyun0714 <seun0714@naver.com>
2025-11-11 16:29:59 +0900

    feat: firestore user 생성, 조회 구현

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,1 +1,24 @@
 // user firestore
+
+import { doc, setDoc, getDoc, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase/config';
+
+const USERS_COLLECTION_NAME = 'users';
+
+// 유저 정보 가져오기
+export const getUser = async (userId) => {
+  const snapshot = await getDoc(doc(db, USERS_COLLECTION_NAME, userId));
+  return {
+    userId: snapshot.id,
+    ...snapshot.data(),
+  };
+};
+
+// 유저 정보 저장(생성) userId: authentication에서 만들어진 uid
+export const createUser = async (userId, userInfo) => {
+  await setDoc(doc(db, USERS_COLLECTION_NAME, userId), {
+    ...userInfo,
+    lectureList: [],
+    createdAt: serverTimestamp(),
+  });
+};
