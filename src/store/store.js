7af2dc7610a8c25dc75b65a0dc07edd81de4ad7e
seyun0714 <seun0714@naver.com>
2025-11-12 14:20:40 +0900

    fix: 새로고침 시 로그아웃 되는 문제 임시 해결

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,29 @@
 // 전역 상태 관리(Slice를 모아서 등록)
 
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
 import userReducer from './userSlice';
+import storageSession from 'redux-persist/lib/storage/session'; // 1. rootReducer 설정
+const rootReducer = combineReducers({
+  user: userReducer,
+  // 다른 리듀서들...
+});
+
+// 2. persist 설정
+const persistConfig = {
+  key: 'root', // 저장소에 저장될 키 이름
+  storage: storageSession, // 로컬 스토리지 사용
+  whitelist: ['user'], // user 리듀서만 저장하겠다 (생략 시 전체 저장)
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    user: userReducer, // {state: { user: { userInfo, loading, error } } }
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
-
+export const persistor = persistStore(store);
 export default store;
