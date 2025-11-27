c2b4ae7856ce3286d4077d067c503938bbb22a2f
seyun0714 <seun0714@naver.com>
2025-11-27 15:18:59 +0900

    refactor: userService(firestore 로직) TS로 변환

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -8,20 +8,24 @@ import {
   updateDoc,
 } from 'firebase/firestore';
 import { db } from '../firebase/config';
+import { User } from 'types/types';
 
 const USERS_COLLECTION_NAME = 'users';
 
 // 유저 정보 가져오기
-export const getUser = async (userId) => {
+export const getUser = async (userId: string): Promise<User> => {
   const snapshot = await getDoc(doc(db, USERS_COLLECTION_NAME, userId));
   return {
     userId: snapshot.id,
     ...snapshot.data(),
-  };
+  } as User;
 };
 
 // 유저 정보 저장(생성) userId: authentication에서 만들어진 uid
-export const createUser = async (userId, userInfo) => {
+export const createUser = async (
+  userId: string,
+  userInfo: Pick<User, 'userEmail' | 'userName' | 'role'>,
+) => {
   await setDoc(doc(db, USERS_COLLECTION_NAME, userId), {
     ...userInfo,
     lectureList: [],
@@ -29,7 +33,10 @@ export const createUser = async (userId, userInfo) => {
   });
 };
 
-export const updateUser = async (userId, userInfo) => {
+export const updateUser = async (
+  userId: string,
+  userInfo: Pick<User, 'lectureList' | 'userName'>,
+) => {
   await updateDoc(doc(db, USERS_COLLECTION_NAME, userId), {
     ...userInfo,
   });
